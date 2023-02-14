35936cb8323b2490741a4eb83dcc2134dcbe3fb2
Bompard Cassandra <cassandra.bompard@etu.univ-lyon.fr>
2023-02-14 16:19:27 +0100

    update display_map function description

--- LifemapR/R/functions.R
+++ LifemapR/R/functions.R
@@ -1,6 +1,7 @@
 #' Create a Lifemap base
 #'
 #' @description This function create a blank Leaflet map using the leaflet function from the leaflet package.
+#' if a dataframe is provided, it will be used for the creation of the map object
 #'
 #' @param map The base choose to display, either \itemize{
 #' - 'fr' for \url{https://lifemap-fr.univ-lyon1.fr/}
@@ -10,13 +11,13 @@
 #' @export
 #'
 #' @examples display_map("fr")
-display_map <- function(map) {
+display_map <- function(df=NULL,map="ncbi") {
   if (map == "fr"){
     display="http://lifemap.univ-lyon1.fr/osm_tiles/{z}/{x}/{y}.png"
   } else if (map == "ncbi"){
     display="http://lifemap-ncbi.univ-lyon1.fr/osm_tiles/{z}/{x}/{y}.png"
     }
-  leaflet::leaflet() %>%
+  leaflet::leaflet(df) %>%
     leaflet::addTiles(display)
 }
 
