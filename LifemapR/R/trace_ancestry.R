f33303573a305724ef1abe6a1ad29511ce87b3a9
Bompard Cassandra <cassandra.bompard@etu.univ-lyon.fr>
2023-02-17 16:08:41 +0100

    reworking of the trace_ancestry function

--- LifemapR/R/trace_ancestry.R
+++ LifemapR/R/trace_ancestry.R
@@ -43,7 +43,7 @@ GetLinFromTaxID <- function(taxids){
   return(DATA)
 }
 
-## get the comon ancestors of all the taxas
+## get the common ancestors of all the taxa as well as the taxa path from the leaf to the ancestor
 get_ancestry <- function(ids){
   data <- GetCooFromTaxID(ids)
   data_lineage <- GetLinFromTaxID(ids)
@@ -66,9 +66,9 @@ get_ancestry <- function(ids){
     lca <- append(lca,element[[2]])
   }
 
-  ancestry <- c(as.numeric(data_final[1,"taxid"]))
+  ancestry_first_tax <- c(as.numeric(data_final[1,"taxid"]))
   for (element in data_final[1,"ascend"][[1]]){
-    ancestry <- append(ancestry,element)
+    ancestry_first_tax <- append(ancestry_first_tax,element)
     if(element %in% lca){
       lca <- setdiff(lca,element)
     }
@@ -76,34 +76,57 @@ get_ancestry <- function(ids){
       break
     }
   }
-  global_ancestry$'1' <- list(ancestry)
-  return(global_ancestry)
-}
+  global_ancestry$'1' <- list(ancestry_first_tax)
 
-get_ancestry_infos <- function(ancestry_info){
-  ancestry_trace <- lapply(ancestry_info, function(x){
-    trace <- list()
-    for (i in 2:length(x[[1]])-1){
-      trace[[as.character(i)]] <- GetCooFromTaxID(c(x[[1]][i],as.character(x[[1]][i+1])))
-    }
-    return(trace)
+  test <- lapply(global_ancestry, function(x){
+    return(x[[1]])
   })
+  test <- unlist(test)
+  ancestors <- as.data.frame(table(test))
+  ancestors <- ancestors[ancestors$Freq >1,]
+  ancestors <- as.vector(ancestors$test)
+  return(list(global_ancestry,ancestors))
 }
 
-add_ancestry <- function(list_of_df, map, ids){
-  for (element in list_of_df){
-    for (i in element){
-      map <- map %>% addPolylines(i$lon, i$lat, color="red") %>%
-        addCircleMarkers(i$lon, i$lat, label=i$taxid, fillColor = "grey", stroke=FALSE)
-      for (tax in 1:2){
-        if (i[tax,"taxid"] %in% ids){
-          map <- map %>% addMarkers(i[tax,"lon"], i[tax,"lat"], label=sprintf("%s,%s",i[tax,"sci_name"],i[tax,"taxid"]))
-        }
-      }
+## setting the color for special nodes
+### the ids requested
+### ancestors common to several of the ids
+getColor <- function(id, ids_want, real_ancestors) {
+  if (id %in% ids_want){
+    "green"
+  } else if (id %in% real_ancestors){
+    "red"
+  }
+}
+
+## add the lines between the taxa in 'df' on 'map'
+add_ancestry <- function(df, map, ids, ancestors){
+  for (row in 1:nrow(df)){
+    map <- map %>% addPolylines(lng=c(df[row,"lon.x"],df[row,"lon.y"]), lat=c(df[row,"lat.x"],df[row,"lat.y"]), color="red") %>%
+      addCircleMarkers(lng=c(df[row,"lon.x"],df[row,"lon.y"]), lat=c(df[row,"lat.x"],df[row,"lat.y"]), label=c(df[row,"tax1"],df[row,"tax2"]), fillColor = "grey", stroke=FALSE)
+
+    if (df[row,"tax1"] %in% ids | df[row,"tax1"] %in% ancestors){
+      icons <- awesomeIcons(
+        icon = 'ios-close',
+        iconColor = 'black',
+        library = 'ion',
+        markerColor = getColor(df[row,"tax1"], ids, ancestors)
+      )
+      map <- map %>% addAwesomeMarkers(lng=df[row,"lon.x"], lat=df[row,"lat.x"], icon=icons, label=df[row,"sci_name.x"])
+    }
+    if (df[row,"tax2"] %in% ids | df[row,"tax2"] %in% ancestors){
+      icons <- awesomeIcons(
+        icon = 'ios-close',
+        iconColor = 'black',
+        library = 'ion',
+        markerColor = getColor(df[row,"tax2"], ids, ancestors)
+      )
+      map <- map %>% addAwesomeMarkers(lng=df[row,"lon.y"], lat=df[row,"lat.y"], icon=icons, label=df[row,"sci_name.y"])
     }
   }
   return(map)
 }
+
 
 
 #' Trace the ancestry of multiple taxa
@@ -116,7 +139,30 @@ add_ancestry <- function(list_of_df, map, ids){
 #' @examples trace_ancestry(c(3641,3649,403667,3394,2))
 trace_ancestry <- function(ids){
   ancestry <- get_ancestry(ids)
-  ancestry_infos <- get_ancestry_infos(ancestry)
+  ancestors <- ancestry[[2]]  # all the taxa's ancestors
+  ancestry <- ancestry[[1]]   # a list of taxids representing the paths from leaf to ancestor for each taxa
+
+  # couple of father-son nodes (to get the correct order when drawing lines)
+  ancestry_couples <- lapply(ancestry, function(x){
+    trace <- list()
+    for (i in 2:length(x[[1]])-1){
+      trace[[as.character(i)]] <- sort(c(x[[1]][i],x[[1]][i+1]))
+    }
+    return(trace)
+  })
+
+  # converting the list into a dataframe
+  df_ancestry <- t(as.data.frame(ancestry_couples))
+  df_ancestry <- unique(df_ancestry)
+  colnames(df_ancestry) <- c("tax1", "tax2")
+
+  # Getting the coordinates for all
+  tax_coo <- GetCooFromTaxID(unique(c(as.vector(df_ancestry[,1]),as.vector(df_ancestry[,2]))))
+
+  df_ancestry <- merge(df_ancestry,tax_coo, by.x="tax1", by.y="taxid")
+  df_ancestry <- merge(df_ancestry,tax_coo, by.x="tax2", by.y="taxid")
+
   m <- display_map()
-  add_ancestry(ancestry_infos,m, ids)
+  add_ancestry(df_ancestry,m, ids, ancestors)
+
 }
