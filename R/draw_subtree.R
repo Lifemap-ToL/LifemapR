81d7af3feb076fac813a2819aaab11c8acf7ce09
Bompard Cassandra <cassbomp68@gmail.com>
2023-03-03 14:46:14 +0100

    not perfect but some lines are traced

--- R/draw_subtree.R
+++ R/draw_subtree.R
@@ -6,28 +6,12 @@ draw_subtree <- function(lm){
   df <- lm[[1]]
   basemap <- lm[[2]]
 
-  map <- display_map(df,basemap) %>%
-    fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
-  for(row in 1:(nrow(df)-1)) {
-    map <- map %>% addPolylines(lng=c(df[row,"lon"],df[df$taxid == df[row,]$ancestor,"lon"]), lat=c(df[row,"lat"],df[df$taxid == df[row,]$ancestor,"lat"]), color="red")
-  }
-
   ui <- fluidPage(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("mymap", width = "100%", height = "1000px")
-    # p(),
-    # absolutePanel(top = 10, right = 20,
-    #               textOutput("zoom"),
-    #               textOutput("boundaries"),
-    #               tags$head(tags$style("#param{color: red;
-    #                              font-size: 20px;
-    #                              font-style: italic;
-    #                              }"
-    #               )
-    #               )
-    # ),
-    # dataTableOutput("new_df"),
-    # dataTableOutput("new_df2")
+    leafletOutput("mymap", width = "100%", height = "1000px"),
+    p(),
+    dataTableOutput("new_df"),
+    dataTableOutput("new_df2")
   )
 
   server <- function(input, output, session) {
@@ -39,73 +23,36 @@ draw_subtree <- function(lm){
            df$lat < input$mymap_bounds$north &
            df$lon > input$mymap_bounds$west &
            df$lon < input$mymap_bounds$east,]
-    )
+      )
 
-    TaxInBounds <- reactive({
-      if (is.null(input$map_bounds))
-        return(df[FALSE,])
-      bounds <- input$mymap_bounds
-      latRng <- range(bounds$north, bounds$south)
-      lngRng <- range(bounds$east, bounds$west)
-
-      subset(df,
-             lat >= latRng[1] & lat <= latRng[2] &
-               lon >= lngRng[1] & lon <= lngRng[2] &
-               zoom < input$mymap_zoom)
+    df_descendants <- reactive({
+      visibles <- df_zoom_bounds()$taxid
+      df[df$ancestor %in% visibles,]
     })
 
-
-
     # output of the map
-    output$mymap <- renderLeaflet({ map })
-
-    # labels <- reactive({
-    #   HTML(paste("<p>", data_input()$Electorate,"</p>",
-    #              "<p>", "Number of Visitors: ", round(data_input()$user_id, digit = 0), "</p>",
-    #              sep = ""))
-    # })
-
+    output$mymap <- renderLeaflet({
+      display_map(df,map = basemap) %>% fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
+    })
 
     # modification of the map to display the rights markers
-    # observe({
-    #   leafletProxy("mymap", session=session, data=df_zoom_bounds()) %>%
-    #     clearMarkers() %>%
-    #     addMarkers(lng=~lon,
-    #                lat=~lat)
-    # })
+    observe({
+      proxy <- leafletProxy("mymap", session=session) %>%
+        clearShapes()
+      for (id in df_zoom_bounds()$taxid) {
+        print(df_zoom_bounds()[df_zoom_bounds()$taxid == id,])
+        print("yo")
+        print(df_descendants()[df_descendants()$ancestor == id,])
+        proxy <- proxy %>%
+          addPolylines(lng=c(df_zoom_bounds()[df_zoom_bounds()$taxid == id,"lon"],df_descendants()[df_descendants()$ancestor == id,"lon"]),
+                       lat=c(df_zoom_bounds()[df_zoom_bounds()$taxid == id,"lat"],df_descendants()[df_descendants()$ancestor == id,"lat"]),
+                       color="red")
+      }
+      proxy
+    })
 
-    # observeEvent(input$mymap_marker_mouseover,{
-    #   req(input$mymap)
-    #   leafletProxy("mymap", data=df_zoom_bounds) %>%
-    #     addPopups(lng=~lon,lat=~lat, popup = ~sci_name)
-    # })
-
-    # show_label <- function(taxid, lat, lng){
-    #   content <- as.character(tagList(
-    #     tags$h4(df$sci_name),
-    #     tags$br,
-    #     sprintf("jean naymar")
-    #     ))
-    #   leafletProxy("mymap") %>% addPopups(lng, lat, content, layerId = zipcode)
-    # }
-
-    # observe({
-    #   leafletProxy("mymap") %>% clearPopups()
-    #   event <- input$map_marker_click
-    #   if (is.null(event))
-    #     return()
-    #
-    #   isolate({
-    #     show_label(event$id, event$lat, event$lng)
-    #   })
-    # })
-
-
-    # output$zoom <- renderText(input$mymap_zoom)
-    # output$boundaries <- renderText(input$mymap_bounds$north)
-    # output$new_df <- renderDataTable(df_zoom_bounds())
-    # output$new_df2 <- renderDataTable(TaxInBounds())
-    # output$param <- renderDataTable(calcul_param())
+    output$new_df <- renderDataTable(df_zoom_bounds())
+    output$new_df2 <- renderDataTable(df_descendants())
 
   }
 
