15a8a36bd418178d0a0393d78eba9f46b688b76b
Bompard Cassandra <cassbomp68@gmail.com>
2023-03-06 16:49:23 +0100

    adding some documentation

--- R/draw_subtree.R
+++ R/draw_subtree.R
@@ -1,6 +1,18 @@
 library(shiny)
 library(leaflet)
 
+#' Draw the subtree of the ToL based on the taxids given
+#'
+#' @param lm_obj a lifemap object
+#'
+#' @return a shiny application
+#' @export
+#'
+#' @examples draw_subtree(LM_df)
+#' df <- read.csv("data/taxids_example.txt", row.names = 1)
+#'
+#' LM_df <- LifemapR::construct_dataframe(df)
+#' draw_subtree(LM_df)
 draw_subtree <- function(lm_obj){
 
   df <- lm_obj[[1]]
@@ -10,8 +22,6 @@ draw_subtree <- function(lm_obj){
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
     leafletOutput("mymap", width = "100%", height = "1000px"),
     p()
-    # dataTableOutput("new_df"),
-    # dataTableOutput("new_df2")
   )
 
   server <- function(input, output, session) {
@@ -25,6 +35,7 @@ draw_subtree <- function(lm_obj){
            df$lon < input$mymap_bounds$east,]
       )
 
+    # define the descendants of df_zoom_bounds taxids
     df_descendants <- reactive({
       visibles <- df_zoom_bounds()$taxid
       df[df$ancestor %in% visibles,]
@@ -40,9 +51,6 @@ draw_subtree <- function(lm_obj){
       proxy <- leafletProxy("mymap", session=session) %>%
         clearShapes()
       for (id in df_zoom_bounds()$taxid) {
-        # print(df_zoom_bounds()[df_zoom_bounds()$taxid == id,])
-        # print("yo")
-        # print(df_descendants()[df_descendants()$ancestor == id,])
         for (desc in df_descendants()[df_descendants()$ancestor == id,]$taxid) {
         proxy <- proxy %>%
           addPolylines(lng=c(df_zoom_bounds()[df_zoom_bounds()$taxid == id,"lon"],df_descendants()[df_descendants()$taxid == desc,"lon"]),
@@ -52,10 +60,6 @@ draw_subtree <- function(lm_obj){
       }
       proxy
     })
-
-    # output$new_df <- renderDataTable(df_zoom_bounds())
-    # output$new_df2 <- renderDataTable(df_descendants())
-
   }
 
   shinyApp(ui, server)
