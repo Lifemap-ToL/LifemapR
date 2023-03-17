82761cdf630b70d2486924cf507b04b29f3ca237
Bompard Cassandra <cassbomp68@gmail.com>
2023-03-17 15:03:19 +0100

    renaming of functions

--- R/build_Lifemap.R
+++ R/build_Lifemap.R
@@ -232,7 +232,7 @@ print.lifemap.obj <- function(lm_obj) {
     cat('The dataframe contains', nrow(lm_obj$df),'rows and', ncol(lm_obj$df), 'columns. \n')
     cat('The basemap used is :', lm_obj$basemap,'\n')
   } else {
-    print(add_Lifemap_markers(lm_obj))
+    print(draw_markers(lm_obj))
   }
 
 }
--- R/draw_markers.R
+++ R/draw_markers.R
@@ -45,15 +45,16 @@ pass_infos <- function(df, information, my_function) {
 #' @return a shiny application
 #' @export
 #' @importFrom dplyr left_join
+#' @importFrom shiny fluidPage reactive observe shinyApp
+#' @importFrom leaflet leafletOutput renderLeaflet fitBounds leafletProxy addCircleMarkers clearMarkers clearShapes clearControls colorNumeric
 #'
 #' @examples
 #' df <- read.csv("data/eukaryotes_1000.txt", header=TRUE, sep="\t")
 #' df_test[,"GC."] <- as.numeric(as.character(df_test[,"GC."]))
 #' df_test[,"Genes"] <- as.numeric(as.character(df_test[,"Genes"]))
 #' LM_df <- construct_dataframe(df)
-#' add_Lifemap_markers(LM_df, information = c("GC.","Genes"), my_function=mean, legend = FALSE)
-add_Lifemap_markers <- function(lm_obj,
-                                legend=TRUE){
+#' draw_markers(LM_df, information = c("GC.","Genes"), my_function=mean, legend = FALSE)
+draw_markers <- function(lm_obj, legend=TRUE){
 
   # df <- lm_obj$df[,c("taxid", information,"lon", "lat", "sci_name", "zoom", "ascend", "genomes", "type", "ancestor")]
   df <- lm_obj$df
@@ -66,6 +67,7 @@ add_Lifemap_markers <- function(lm_obj,
     # si pas d'info de couleur, on rempli aes
     # current_row <- aes[i,]
 
+    # passing informations if the function is given
     if (!(is.null(aes[i,"pass_info"]))) {
       new_df <- pass_infos(df,information=aes[i,"obj"], my_function=aes[i,"pass_info"])
       for (id in 1:nrow(new_df)) {
@@ -73,21 +75,22 @@ add_Lifemap_markers <- function(lm_obj,
       }
     }
 
+    # calculating the ranges of values for marker's size
     aes$OldRange <- max(df[[aes[i,"obj"]]],na.rm = TRUE) - min(df[[aes[i,"obj"]]],na.rm = TRUE)
     aes$NewRange <- aes[i,"max"] - aes[i,"min"]
 
   }
 
-  ui <- fluidPage(
+  ui <- shiny::fluidPage(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("mymap", width = "100%", height = "1000px"),
+    leaflet::leafletOutput("mymap", width = "100%", height = "1000px"),
     p()
   )
 
   server <- function(input, output, session) {
 
     # define the zone visible by the users
-    df_zoom_bounds <- reactive(
+    df_zoom_bounds <- shiny::reactive(
       df[df$zoom <= (input$mymap_zoom +5) &
            df$lat > input$mymap_bounds$south &
            df$lat < input$mymap_bounds$north &
@@ -96,78 +99,78 @@ add_Lifemap_markers <- function(lm_obj,
     )
 
     # output of the map
-    output$mymap <- renderLeaflet({
-      display_map(df,map = basemap) %>% fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
+    output$mymap <- leaflet::renderLeaflet({
+      display_map(df,map = basemap) %>% leaflet::fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
     })
 
     # modification of the map to display the rights markers
-    observe({
-      proxy <- leafletProxy("mymap", session=session) %>%
-        clearShapes() %>%
-        clearMarkers() %>%
-        clearControls()
+    shiny::observe({
+      proxy <- leaflet::leafletProxy("mymap", session=session) %>%
+        leaflet::clearShapes() %>%
+        leaflet::clearMarkers() %>%
+        leaflet::clearControls()
 
       for (i in 1:nrow(aes)){
 
         if (aes[i,"by"] == "size") {
           radius_info <- (((df_zoom_bounds()[[aes[i,"obj"]]] -1) * aes[i,"NewRange"]) / aes[i,"OldRange"]) + aes[i,"min"]
           fillCol_info <- aes[i,"col"]
-          proxy <- addCircleMarkers(proxy, lng=df_zoom_bounds()$lon,
+          proxy <- leaflet::addCircleMarkers(proxy, lng=df_zoom_bounds()$lon,
                                     lat=df_zoom_bounds()$lat,
                                     radius=radius_info,
                                     fillColor = fillCol_info,
                                     fillOpacity = 0.5,
                                     stroke=FALSE)
+          # if (legend == TRUE) {
+          #   proxy <- leaflegend::addLegendSize(proxy,
+          #                                      values=df_zoom_bounds()[[aes[i,"obj"]]],
+          #                                      color=aes[i,"col"],
+          #                                      title = aes[i,"obj"],
+          #                                      shape = "circle")
+          # }
         } else if (aes[i,"by"] == "color") {
-          pal <- colorNumeric(aes[i,"col"], df[[aes[i,"obj"]]])
+          pal <- leaflet::colorNumeric(aes[i,"col"], df[[aes[i,"obj"]]])
           radius_info <- aes[i,"min"]
-          proxy <- addCircleMarkers(proxy, lng=df_zoom_bounds()$lon,
+          proxy <- leaflet::addCircleMarkers(proxy, lng=df_zoom_bounds()$lon,
                                     lat=df_zoom_bounds()$lat,
                                     radius=radius_info,
                                     fillColor = pal(df_zoom_bounds()[[aes[i,"obj"]]]),
                                     fillOpacity = 0.5,
                                     stroke=FALSE)
+          if (legend == TRUE) {
+              proxy <- leaflet::addLegend(proxy,
+                                          position = "bottomright",
+                                          title=aes[i,"obj"],
+                                          pal = pal,
+                                          values = df_zoom_bounds()[[aes[i,"obj"]]])
+          }
         }
-
-        # proxy <- addCircleMarkers(proxy, lng=df_zoom_bounds()$lon,
-        #                           lat=df_zoom_bounds()$lat,
-        #                           radius=radius_info,
-        #                           fillColor = fillCol_info(df_zoom_bounds()[[aes[i,"obj"]]]),
-        #                           fillOpacity = 0.5,
-        #                           stroke=FALSE)
-        # if (legend == TRUE) {
-        #   proxy <- addLegend(proxy, position = "bottomright", title=information,
-        #                      pal = pal, values = df_zoom_bounds()[[information]])
-        #
-        # }
       }
-      # if (by == "size" && legend == TRUE) {
-      #   proxy <- addLegend(proxy, position = "bottomright", title="size",
-      #                      colors = col_info, labels = information)
-      # }
       proxy
     })
 
-    # showSciName <- function(taxid, lng, lat) {
-    #   selectedId <- df[round(df$lon, digits=6) == round(lng,digits=6) & round(df$lat, digits=6) == round(lat, digits = 6),]
-    #   content <- as.character(selectedId$taxid)
-    #   for (i in 1:length(information)) {
-    #     new_string <- paste(information[i]," : ", selectedId[[information[i]]], sep="")
-    #     content <- paste(content,new_string, sep="\n")
-    #   }
-    #   leafletProxy("mymap") %>% addPopups(lng, lat, content)
-    # }
-    #
-    # observe({
-    #   leafletProxy("mymap") %>% clearPopups()
-    #   event <- input$mymap_marker_click
-    #   if (is.null(event))
-    #     return()
-    #
-    #   isolate({
-    #     showSciName(event$id, event$lng, event$lat)
-    #   })
-    # })
+    # functions to add popups
+    showSciName <- function(taxid, lng, lat) {
+      selectedId <- df[round(df$lon, digits=6) == round(lng,digits=6) & round(df$lat, digits=6) == round(lat, digits = 6),]
+      content <- as.character(selectedId$taxid)
+      for (i in 1:nrow(aes)) {
+        new_string <- paste(aes[i, "obj"]," : ", selectedId[[aes[i,"obj"]]], sep="")
+        content <- paste(content,new_string, sep="\n")
+      }
+      leafletProxy("mymap") %>% addPopups(lng, lat, content)
+    }
+
+    # when clicking on a marker, show a popup
+    observe({
+      leafletProxy("mymap") %>% clearPopups()
+      event <- input$mymap_marker_click
+      if (is.null(event))
+        return()
+
+      isolate({
+        showSciName(event$id, event$lng, event$lat)
+      })
+    })
 
   }
 
