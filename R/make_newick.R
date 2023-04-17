23f8b891f81676b2b14e13a7aef6d4d062dd2dda
Bompard Cassandra <cassbomp68@gmail.com>
2023-04-17 15:03:56 +0200

    adding make_newick function

---
+++ R/make_newick.R
@@ -0,0 +1,43 @@
+#' Create a newick for the given dataset
+#'
+#' @param df a dataframe either enriched with by the build_Lifemap function or containing at least two columns :
+#' - "taxid" containing NCBI format taxids
+#' - "ancestor" containing for each taxid, it's direct ancestor in the phyogeny
+#'
+#' @returna character string
+#' @export
+#'
+#' @examples
+#'
+#' LM <- build_Lifemap(eukaryotes_1000)
+#' make_newick(LM$df)
+make_newick<-function(df) { #df must contain, a minima, a column "taxid" and a column "ancestor"
+  mat<-cbind(df$ancestor, df$taxid)
+
+  #remove NA (if any)
+  mat<-mat[complete.cases(mat),]
+
+  # put the root edges as first rows
+  whereroot<-which(is.na(match(mat[,1],mat[,2])))
+  mat<-rbind(mat[whereroot,],mat[-whereroot,])
+  i<-length(whereroot)+1
+  while (i<nrow(mat)) {
+    while(!(mat[i,1] %in% mat[1:(i-1),2])) {
+      mat<-rbind(mat[-i,],mat[i,])
+    }
+    i<-i+1
+  }
+
+  ## Put secial characters ("-") before and after each name
+  ## for preventing errors with gsub afterwards.
+  matok<-t(apply(mat,1,function(x) paste("-",x,"-", sep="")))
+  nodes<-unique(matok[,1])
+  nwk<-paste(matok[1,2],";", sep="")
+  for (n in nodes[-1]) {
+    desc<-paste("(",paste(matok[which(matok[,1]%in% n),2], collapse=","),")",n,sep="")
+    nwk<-gsub(n,desc,nwk)
+  }
+  #remove the special character
+  NWK<-gsub("-","",nwk)
+  NWK
+}
